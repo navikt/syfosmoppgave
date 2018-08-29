c200a2d6d493652b0064a112f7dc5b971dd3e8e7
Joakim kartveit <joakim.kartveit@nav.no>
2018-08-29 10:56:23 +0200

    Glemte ein g i servicebrukren

--- src/main/kotlin/no/nav/syfo/Environment.kt
+++ src/main/kotlin/no/nav/syfo/Environment.kt
@@ -3,8 +3,8 @@ package no.nav.syfo
 data class Environment(
         val applicationPort: Int = getEnvVar("APPLICATION_PORT", "8080").toInt(),
         val applicationThreads: Int = getEnvVar("APPLICATION_THREADS", "4").toInt(),
-        val srvsyfooppgavegsakUsername: String = getEnvVar("SRVSYFOOPPGAVESAK_USERNAME"),
-        val srvsyfooppgavegsakPassword: String = getEnvVar("SRVSYFOOPPGAVESAK_PASSWORD"),
+        val srvsyfooppgavegsakUsername: String = getEnvVar("SRVSYFOOPPGAVEGSAK_USERNAME"),
+        val srvsyfooppgavegsakPassword: String = getEnvVar("SRVSYFOOPPGAVEGSAK_PASSWORD"),
         val kafkaBootstrapServers: String = getEnvVar("KAFKA_BOOTSTRAP_SERVERS_URL"),
         val kafkaSM2013OppgaveGsakTopic: String = getEnvVar("KAFKA_SM2013_OPPGAVE_TOPIC", "privat-syfomottak-sm2013-oppgaveGsak")
 )
