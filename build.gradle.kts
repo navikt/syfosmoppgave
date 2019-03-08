7e0311f9756b66033c3d8b86b293d0d4a3d78044
Joakim kartveit <joakim.kartveit@nav.no>
2019-03-08 11:07:29 +0100

    Added showStandardStreams

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ tasks.withType<Test> {
     useJUnitPlatform {
         includeEngines("spek2")
     }
-    testLogging.events("passed", "skipped", "failed")
+    testLogging.showStandardStreams = true
 }
 
 tasks.create("printVersion") {
