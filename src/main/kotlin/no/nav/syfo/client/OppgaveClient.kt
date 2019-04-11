a1f0e93cc02190b7f85d717f7ec9bb2c6899d03e
Joakim kartveit <joakim.kartveit@nav.no>
2019-04-11 12:22:50 +0200

    Removed unused import

--- src/main/kotlin/no/nav/syfo/client/OppgaveClient.kt
+++ src/main/kotlin/no/nav/syfo/client/OppgaveClient.kt
@@ -1,6 +1,5 @@
 package no.nav.syfo.client
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
