95fac91292cc8e42001c7682ec69372cd76f1b7c
Joakim kartveit <joakim.kartveit@nav.no>
2019-04-12 11:34:16 +0200

    Removed accept

--- src/main/kotlin/no/nav/syfo/client/OppgaveClient.kt
+++ src/main/kotlin/no/nav/syfo/client/OppgaveClient.kt
@@ -13,11 +13,11 @@ import io.ktor.client.features.logging.DEFAULT
 import io.ktor.client.features.logging.LogLevel
 import io.ktor.client.features.logging.Logger
 import io.ktor.client.features.logging.Logging
-import io.ktor.client.request.accept
 import io.ktor.client.request.header
 import io.ktor.client.request.post
 import io.ktor.client.response.HttpResponse
 import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import io.ktor.util.KtorExperimentalAPI
 import no.nav.syfo.helpers.retry
 import no.nav.syfo.model.OppgaveResponse
@@ -43,7 +43,7 @@ class OppgaveClient constructor(val url: String, val oidcClient: StsOidcClient) 
     suspend fun createOppgave(createOppgave: OpprettOppgave): OppgaveResponse = retry("create_oppgave") {
         // TODO: Remove this workaround whenever ktor issue #1009 is fixed
         client.post<HttpResponse>(url) {
-            accept(ContentType.Application.Json)
+            contentType(ContentType.Application.Json)
             val oidcToken = oidcClient.oidcToken()
             this.header("Authorization", "Bearer ${oidcToken.access_token}")
             this.header("X-Correlation-ID", createOppgave.saksreferanse)
