2c7f21eec612b945c7988c9d4b817c0680478a0d
Joakim Kartveit <joakimkartveit@gmail.com>
2019-05-29 09:52:00 +0200

    Oppdatere ktor til 1.2.0, ktor problem nr #1009 er fikset

--- src/main/kotlin/no/nav/syfo/client/OppgaveClient.kt
+++ src/main/kotlin/no/nav/syfo/client/OppgaveClient.kt
@@ -5,13 +5,11 @@ import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.ktor.client.HttpClient
-import io.ktor.client.call.receive
 import io.ktor.client.engine.apache.Apache
 import io.ktor.client.features.json.JacksonSerializer
 import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.request.header
 import io.ktor.client.request.post
-import io.ktor.client.response.HttpResponse
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import io.ktor.util.KtorExperimentalAPI
@@ -33,13 +31,12 @@ class OppgaveClient constructor(val url: String, val oidcClient: StsOidcClient) 
     }
 
     suspend fun createOppgave(createOppgave: OpprettOppgave, msgId: String): OppgaveResponse = retry("create_oppgave") {
-        // TODO: Remove this workaround whenever ktor issue #1009 is fixed
-        client.post<HttpResponse>(url) {
+        client.post<OppgaveResponse>(url) {
             contentType(ContentType.Application.Json)
             val oidcToken = oidcClient.oidcToken()
             this.header("Authorization", "Bearer ${oidcToken.access_token}")
             this.header("X-Correlation-ID", msgId)
             body = createOppgave
-        }.use { it.call.response.receive<OppgaveResponse>() }
+        }
     }
 }
