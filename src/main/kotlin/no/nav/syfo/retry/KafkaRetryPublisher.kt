b71a6e19e6fe3e399c70cf4689c2c8f10de79e58
Mathias Hellevang <mathias.hellevang@gmail.com>
2020-03-24 15:10:34 +0100

    Ta til en metrikk over antall oppgaver som sendes til kafka retry topic

--- src/main/kotlin/no/nav/syfo/retry/KafkaRetryPublisher.kt
+++ src/main/kotlin/no/nav/syfo/retry/KafkaRetryPublisher.kt
@@ -3,6 +3,7 @@ package no.nav.syfo.retry
 import net.logstash.logback.argument.StructuredArguments
 import no.nav.syfo.LoggingMeta
 import no.nav.syfo.log
+import no.nav.syfo.metrics.RETRY_OPPGAVE_COUNTER
 import no.nav.syfo.model.OpprettOppgave
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
@@ -12,5 +13,6 @@ class KafkaRetryPublisher(private val kafkaRetryProducer: KafkaProducer<String, 
         val kafkaMessage = OppgaveRetryKafkaMessage(loggingMeta, opprettOppgave)
         kafkaRetryProducer.send(ProducerRecord(retryTopic, messageId, kafkaMessage))
         log.info("publish to retrytopic for opprett oppgave {}", StructuredArguments.fields(loggingMeta))
+        RETRY_OPPGAVE_COUNTER.inc()
     }
 }
