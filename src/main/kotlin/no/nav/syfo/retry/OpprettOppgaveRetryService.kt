3b7aca56ea9c5d497177f36381cff37a3e8d6d2a
andreasnilsen <danduras@gmail.com>
2020-11-12 08:52:13 +0100

    skipping old tasks

--- src/main/kotlin/no/nav/syfo/retry/OpprettOppgaveRetryService.kt
+++ src/main/kotlin/no/nav/syfo/retry/OpprettOppgaveRetryService.kt
@@ -13,6 +13,7 @@ import no.nav.syfo.retry.util.getNextRunTime
 import no.nav.syfo.service.opprettOppgave
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.slf4j.LoggerFactory
+import java.time.Instant
 
 @KtorExperimentalAPI
 class OpprettOppgaveRetryService(
@@ -44,8 +45,12 @@ class OpprettOppgaveRetryService(
             records.forEach {
                 val kafkaMessage = it.value()
                 val messageId = it.key()
-                log.info("Running retry for opprett oppgave {}", fields(kafkaMessage.loggingMeta))
-                opprettOppgave(oppgaveClient, kafkaMessage.opprettOppgave, kafkaMessage.loggingMeta, messageId = messageId)
+                if(Instant.ofEpochMilli(it.timestamp()).atOffset(ZoneOffset.UTC) < OffsetDateTime.of(2020, 11, 8, 0, 0, 0, 0, ZoneOffset.UTC)) {
+                    log.warn("skipping oppgave ${kafkaMessage.opprettOppgave}")
+                } else {
+                    log.info("Running retry for opprett oppgave {}", fields(kafkaMessage.loggingMeta))
+                    opprettOppgave(oppgaveClient, kafkaMessage.opprettOppgave, kafkaMessage.loggingMeta, messageId = messageId)
+                }
             }
             if (!records.isEmpty) {
                 endTime = OffsetDateTime.now(ZoneOffset.UTC).plusMinutes(5)
